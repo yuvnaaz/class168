a07923d4c35553698e525ac967c67d6478da65cf
yuvnaaz <69728010+yuvnaaz@users.noreply.github.com>
2022-07-29 17:56:05 -0700

    Add files via upload

--- markerHandler.js
+++ markerHandler.js
@@ -1,10 +1,31 @@
 AFRAME.registerComponent("handler",{
     init: async function(){
         this.el.addEventListener("markerFound", ()=>{
-            console.log("markerFound")
+            var buttonDiv = document.getElementById("button-div")
+            buttonDiv.style.display = "flex"
+            var rating = document.getElementById("rating-button")
+            var order = document.getElementById("order-button")
+
+            rating.addEventListener("click", function(){
+                swal({
+                    icon: "warning",
+                    title: "Rating",
+                    text: "Work In Progress"
+                })
+            })
+            order.addEventListener("click", function(){
+                swal({
+                    title: "Order",
+                    icon: "success",
+                    text: "Order Successfully Placed!"
+                })
+            })
         })
         this.el.addEventListener("markerLost",()=>{
-            console.log("markerLost")
+        var buttonDiv = document.getElementById("button-div")
+        buttonDiv.style.display = "none"
+
+
         })
 
         
